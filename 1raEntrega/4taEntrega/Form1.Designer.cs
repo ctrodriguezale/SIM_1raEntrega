ea43404251214c2154a2cf7ff60b10818902f3bb
GimeVidela <gimevmprs@gmail.com>
2019-09-07 20:11:06 -0300

    Creacion de Proyecto 4: Batalla Naval
    Se crea pantalla principal, las pantallas para el juego de manera automatica y semiautomatica y la clase para guardar todas las funciones pertinente.

---
+++ 1raEntrega/4taEntrega/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace _4taEntrega
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imgIcono = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ejecutarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.batallaNavalModoAutomaticoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.batallaNavalModoSemiautomáticoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 155);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(167, 127);
+            this.richTextBox1.TabIndex = 24;
+            this.richTextBox1.Text = "- Baez Lucas (58584)\n\n- Floreano Micaela (72276)\n\n- Ribotta Franco (74120)\n\n- Rod" +
+    "riguez Alejandro (61142)\n\n- Videla Gimena (66984)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Grupo Nº 6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(410, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Trabajo Práctico Nº4: Estrategias de Batalla Naval";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 42);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Simulación ";
+            // 
+            // imgIcono
+            // 
+            this.imgIcono.Image = ((System.Drawing.Image)(resources.GetObject("imgIcono.Image")));
+            this.imgIcono.Location = new System.Drawing.Point(255, 22);
+            this.imgIcono.Name = "imgIcono";
+            this.imgIcono.Size = new System.Drawing.Size(202, 42);
+            this.imgIcono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgIcono.TabIndex = 20;
+            this.imgIcono.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(382, 290);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 19;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ejecutarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(480, 24);
+            this.menuStrip1.TabIndex = 25;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ejecutarToolStripMenuItem
+            // 
+            this.ejecutarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.batallaNavalModoAutomaticoToolStripMenuItem,
+            this.batallaNavalModoSemiautomáticoToolStripMenuItem});
+            this.ejecutarToolStripMenuItem.Name = "ejecutarToolStripMenuItem";
+            this.ejecutarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.ejecutarToolStripMenuItem.Text = "Ejecutar";
+            // 
+            // batallaNavalModoAutomaticoToolStripMenuItem
+            // 
+            this.batallaNavalModoAutomaticoToolStripMenuItem.Name = "batallaNavalModoAutomaticoToolStripMenuItem";
+            this.batallaNavalModoAutomaticoToolStripMenuItem.Size = new System.Drawing.Size(270, 22);
+            this.batallaNavalModoAutomaticoToolStripMenuItem.Text = "Batalla Naval: Modo Automático";
+            // 
+            // batallaNavalModoSemiautomáticoToolStripMenuItem
+            // 
+            this.batallaNavalModoSemiautomáticoToolStripMenuItem.Name = "batallaNavalModoSemiautomáticoToolStripMenuItem";
+            this.batallaNavalModoSemiautomáticoToolStripMenuItem.Size = new System.Drawing.Size(270, 22);
+            this.batallaNavalModoSemiautomáticoToolStripMenuItem.Text = "Batalla Naval: Modo Semiautomático";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 326);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgIcono);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Principal";
+            this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcono)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox imgIcono;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ejecutarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem batallaNavalModoAutomaticoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem batallaNavalModoSemiautomáticoToolStripMenuItem;
+    }
+}
+
